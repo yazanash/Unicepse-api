b2aeeecebda512d6c119efc192515e25437999e6
yazanash <yazan.ash.doonaas@gmail.com@gmail.com>
2024-06-12 19:41:40 +0300

    conmongo

--- db.py
+++ db.py
@@ -1,6 +1,5 @@
 from pymongo import mongo_client
 from pymongo.server_api import ServerApi
-# client = mongo_client.MongoClient("localhost", 27017)
 
 uri = "mongodb+srv://yazankaboshash:SZuSEJKcBRAk6qYK@cluster0.aksycvo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 # Create a new client and connect to the server
