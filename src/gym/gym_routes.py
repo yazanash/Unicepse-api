5ff9b15528cb10f125ff9751aabb67c9786ef4db
yazanash <yazan.ash.doonaas@gmail.com>
2024-09-25 12:12:34 +0300

    gym services

--- src/gym/gym_routes.py
+++ src/gym/gym_routes.py
@@ -24,7 +24,7 @@ def read_gyms():
     return service.read_gyms_use_case()
 
 
-@gyms_bp.route(f"{route}/get", methods=["GET"])
+@gyms_bp.route(f"{route}/shakes", methods=["GET"])
 @token_required
 def read_user_gyms(current_user):
     return service.read_user_gyms_use_case(current_user)
